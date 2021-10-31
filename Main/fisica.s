c1df980fbf6a3f599a9a6b5d584541ee65b5312d
Anon1929 <anon89345@outlook.com>
2021-10-31 11:14:55 -0300

    Fisica horizontal

--- Main/fisica.s
+++ Main/fisica.s
@@ -1,7 +1,75 @@
+.eqv gravity_acc 1
+.eqv v_dash
+.eqv h_resist 1
+.eqv v_resist 1
+.eqv h_acc    3
+.eqv v_acc    3
+.eqv h_max 8
+.eqv v_max 12
 FISICA:
-    addi sp, sp, -4 
-	sw ra, 0(sp)
+    addi sp, sp, -4
+    sw ra, 0(sp)
+
+    lw t0, (a0)  #h_state (-1,0,1)
+    lw t1, 4(a0) #v_state (-1,0,1)
+    lw t2, 8(a0) #grounded
+    lw t3, 12(a0) #dash
+    lw t4, (a1) #h_speed
+    lw t5, 4(a1) #v_speed)
+     # atrito horizontal e vertical, velocidades máximas, gravidade 
+     #
+     #     
+     
+    addi t5, t5, gravity_acc
+    bgt  t0, zero, move_right
+    blt  t0, zero, move_left
+    
+stop_h:  bgt t4, zero, stop_h_right
+	 blt t4, zero, stop_h_left
+	 j air
+stop_h_right: addi t4,t4, -h_resist
+	j air
+stop_h_left: addi t4,t4, h_resist
+        j air                                                           
+                            
+move_right: addi t4,t4, v_acc
+	j stop_h
+move_left:  addi t4,t4 -vacc
+	j stop                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+air:
+
+      
+  
+    
+    
+		
+fim_fis:                #checagem de velocidades , t4=hspeed t5=vspeed
+     #h check
+    li t6, h_max 
+    bgt  t4,t6, h_over
+    li t6, -h_max
+    blt t4, t6, h_over
+    #v check
+ v_check:   li t6, v_max 
+    bgt  t5,t6, v_over
+    li t6, -h_max
+    blt t4, t6, v_over
+    j no_check
+
+h_over: mv t4, t6
+	j v_check
+v_over: mv t5, t6
+
     #Logica da FISICA
+no_check:
+    #recolocada de valores
+    sw t4, (a1) #h_speed
+    
+    
+    
+    
     lw ra, 0(sp)
     addi sp, sp, 4
+    
     ret
