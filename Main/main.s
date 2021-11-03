a2dc60207b16806de4a9bbbb9447db41358eb9fc
Davi <48662592+d-magit@users.noreply.github.com>
2021-11-03 04:12:14 -0300

    Tudo pronto, menos o q tá na descrição
    Falta inimigo.
    Falta também arrumar o magenta dos .bin. Por algum motivo não funciona no FPGRars, mas funciona no Rars normal, n faz sentido ALGUM.

--- Main/main.s
+++ Main/main.s
@@ -16,38 +16,33 @@ player_low: .space 256
 character: .string "playerlow.bin"
 character_low: .string "player.bin"
 
-
-
-
-portrait: .string "celeste.bin"
 lastTime: .word 0
 end: .string "../fases/end.bin"
 #fase 1
 fase1: .string "../fases/fase2.bin"
 colider1: .string "../fases/fase2colider.bin"
 spawn1: .word 20, 140
-STR1: .string "Cacete, que merda de controle"
+texto1: .string "../fases/fase2txt.bin"
 #fase 2
 fase2: .string "../fases/fase4.bin"
 colider2: .string "../fases/fase4colider.bin"
 spawn2: .word 20, 140
-STR2: .string "Modo Turbo"
+texto2: .string "../fases/fase4txt.bin"
 #fase 3
 fase3: .string "../fases/fase1.bin"
 colider3: .string "../fases/fase1colider.bin"
 spawn3: .word 20, 160
-STR3: .string "sandy"
+texto3: .string "../fases/fase1txt.bin"
 #fase 4
 fase4: .string "../fases/fase3.bin"
 colider4: .string "../fases/fase3colider.bin"
 spawn4: .word 20, 160
-STR4: .string "aaa"
+texto4: .string "../fases/fase3txt.bin"
 #fase5
 fase5: .string "../fases/fase5.bin"
 colider5: .string "../fases/fase5colider.bin"
 spawn5: .word 20, 180
-STR5: .string "sanfvg"
-#
+texto5: .string "../fases/fase5txt.bin"
 fases: .word 0,0,0,0 0,0,0,0 0,0,0,0 0,0,0,0 0,0,0,0
 fase_atual: .word 0
 spawn: .word 0, 0
@@ -66,7 +61,7 @@ morangos: .word 0
 	sw t1, 4(t0)
 	la t1, spawn1
 	sw t1, 8(t0)
-	la t1, STR1
+	la t1, texto1
 	sw t1, 12(t0)
 	addi t0, t0, 16
 	#fase2
@@ -76,7 +71,7 @@ morangos: .word 0
 	sw t1, 4(t0)
 	la t1, spawn2
 	sw t1, 8(t0)
-	la t1, STR2
+	la t1, texto2
 	sw t1, 12(t0)
 	addi t0, t0, 16
 	#fase3
@@ -86,7 +81,7 @@ morangos: .word 0
 	sw t1, 4(t0)
 	la t1, spawn3
 	sw t1, 8(t0)
-	la t1, STR3
+	la t1, texto3
 	sw t1, 12(t0)
 	addi t0, t0, 16
 	#fase4
@@ -96,7 +91,7 @@ morangos: .word 0
 	sw t1, 4(t0)
 	la t1, spawn4
 	sw t1, 8(t0)
-	la t1, STR4
+	la t1, texto4
 	sw t1, 12(t0)
 	addi t0, t0, 16
 	#fase5
@@ -106,13 +101,18 @@ morangos: .word 0
 	sw t1, 4(t0)
 	la t1, spawn5
 	sw t1, 8(t0)
-	la t1, STR5
+	la t1, texto5
 	sw t1, 12(t0)
 lv_start:
 	la t0, morango_enable
 	li t1, 1
 	sw t1, 0(t0) #liberar pegar morango
+	
+	la t0, fase_atual
+	lw t0, 0(t0)
+	li t1, 5
 	beq t0, t1, EndGame
+	
 	call LOAD_IMAGES #carrega background e player
 	call DIALOG
 	call LOAD_IMAGES
@@ -132,6 +132,7 @@ game_loop:
 	lw t0, 0(t0)
 	bnez t0 skip_music
 	mv a0, s9	# a0 = dT
+
 	call MUSIC_CALL
 skip_music:
 
@@ -237,7 +238,7 @@ LOAD_IMAGES:
 	li a7 57
 	ecall #close file
 	
-		#load player
+	#load player
 	#open
 	la a0, character_low
 	li a1, 0
@@ -346,30 +347,13 @@ LOAD_IMAGES:
 DIALOG:
 	addi sp, sp, -4
 	sw ra 0(sp)
-	li a0, 0
-	li a1, 180
-	li a3, 0
-	call DRAW_RECT
 	
-	#texto
-	li a7 104
-	li a1 80
-	li a2 200
-	li a3, 0x0038	
-	la a0, fases
-	la t0, fase_atual
-	lw t0, 0(t0)
-	li t1, 16
-	mul t0,t0,t1
-	add a0, a0, t0
-	lw a0, 12(a0)
-	ecall 
-	#texto
-	
-	li a0,0
+		
 	li a1, 180
 	la a3, portrait_buff
 	call DRAW_SPRITE_DIALOG
+	
+	
 	lw ra, 0(sp)
 	addi sp, sp, 4
 
@@ -380,29 +364,7 @@ LOOP_J:	#check for j
 	bne t2, t0, LOOP_J
 	
 	ret
-DRAW_RECT:
-	addi sp, sp, -4 
-	sw ra, 0(sp)
-	li t0,0xFF000000 #endereco inicial do frame 0
-	li t1, 320
-	mul a1, a1, t1
-	add t0, t0, a1 #adiciona o y
-	add t0, t0, a0 #adiciona o x
-	li t2, 20480 #320*16
-	add t2, t0, t2 #inicio+height*screenwidth
-loop_y: beq t0, t2, FIM_DRAW #Checa se já passou da última fileira
-	addi t1, t0, 320 #inicio + width
-loop_x:	beq t0, t1, FORA_X_DRAW
-	#substituir por lw t3, 0()
-	sw a3, 0(t0) #Salvar t3 na memoria de video
-	addi t0, t0, 4 #próxima word
-	j loop_x
-FORA_X_DRAW: addi t0, t0, 0	#320-16 (Proxima linha)
-	j loop_y
-FIM_DRAW: 
-	lw ra, 0(sp)
-	addi sp, sp, 4
-	ret	
+
 	
 DRAW_SPRITE_DIALOG:
 	addi sp, sp, -4 
@@ -413,7 +375,13 @@ DRAW_SPRITE_DIALOG:
 	add t3, t3, a1
 	
 	#open
-	la a0, portrait
+	la a0, fases
+	la t0, fase_atual
+	lw t0, 0(t0)
+	li t1, 16
+	mul t0,t0,t1
+	add a0, a0, t0
+	lw a0, 12(a0)
 	li a1, 0
 	li a7, 1024
 	ecall#open file
@@ -425,7 +393,7 @@ DRAW_SPRITE_DIALOG:
 loopD:	
 	mv a0, t0
 	mv a1, t3
-	li a2, 60 #size
+	li a2, 320 #size
 	li a7, 63 #read file
 	ecall
 	addi t3,t3,320
